afd242f773c8d3c2708dd2c8b728c0baaad79bca
M-yoshimura-ML <masahiro.yoshimura@freemight.com>
2022-08-11 20:50:05 +0800

    SSR getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -24,11 +24,23 @@ export default function HomePage (props) {
   )
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
+  // fetch data from API
+  
   return {
     props: {
       meetups: DUMMY_MEETUPS
-    },
-    revalidate: 10
+    }
   }
 }
+
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS
+//     },
+//     revalidate: 10
+//   }
+// }
